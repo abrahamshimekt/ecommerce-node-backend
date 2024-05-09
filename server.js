11e05ee307900caa11e8e1b7b10784b2516fa5f7
abrahamshimekt <abrahamshimekt4096@gmail.com>
2024-05-09 20:51:51 +0300

    configure server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const cors = require('cors')
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+const ordersRoutes = require("./routes/order");
+const paymentRoutes = require("./routes/stripe");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const mongoose = require('mongoose');
+const { error } = require('console');
+mongoose.connect(process.env.MONGO_DB_URL).then(()=>console.log("connected to db")).catch((error)=>console.log(error));
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/users", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/orders", ordersRoutes);
+app.use("/api/checkout", paymentRoutes);
+
+
+app.get("/", (req, res) => {
+    res.send("RESTFULL APIs FOR SHOP")
+})
+
+app.listen(PORT,()=>{
+    console.log(`App listening at port ${PORT}`);
+})
